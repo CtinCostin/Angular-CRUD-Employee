55996da4338ff977f1b91e4e93e90baad1783452
Costi <costi_georgescu2000@yahho.com>
2022-09-15 18:41:29 +0300

    app finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-CRUD-Employee';
+  title = 'Angular Spring Boot CRUD Employee Full Stack App';
 }
